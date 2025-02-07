1d782a18fabcb049c348efeeb0d449ce348561a1
Martynas Bielskus <bmartynas99@gmail.com>
2025-02-07 19:27:44 +0200

    build: add args parsing

--- pkgs/build/src/main.c
+++ pkgs/build/src/main.c
@@ -1,6 +1,37 @@
-#include "libbuild.h"
+#include "args.h"
+#include "print.h"
 
-int main()
+#include <stdio.h>
+
+typedef enum gen_e {
+	GEN_MAKE,
+} gen_t;
+
+int main(int argc, const char **argv)
 {
-	return libbuild();
+	const char *source = ".";
+	const char *build  = "./bin";
+	gen_t gen	   = GEN_MAKE;
+
+	const opt_enum_val_t gens[] = {
+		[GEN_MAKE]  = {"M", "Make"},
+	};
+
+	const opt_enum_t gens_desc = {
+		.name	   = "Generators",
+		.vals	   = gens,
+		.vals_size = sizeof(gens),
+	};
+
+	opt_t opts[] = {
+		OPT('s', "source", OPT_STR, "<path>", "Specify source directory", &source, {0}, OPT_OPT),
+		OPT('b', "build", OPT_STR, "<path>", "Specify build directory", &build, {0}, OPT_OPT),
+		OPT('g', "generator", OPT_ENUM, "<generator>", "Specify build system generator", &gen, gens_desc, OPT_OPT),
+	};
+
+	if (args_parse(argc, argv, opts, sizeof(opts), PRINT_DST_STD())) {
+		return 1;
+	}
+
+	return 0;
 }
